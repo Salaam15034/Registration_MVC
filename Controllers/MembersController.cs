aae0fe08f06d3033eab66a82aec95e945754a48c
Salaam-PC\Wallero <mohammed.salaam@wallero.com>
2019-08-05 15:47:55 +0530

    adding controllers

---
+++ Controllers/MembersController.cs
@@ -0,0 +1,18 @@
+﻿using Login_Registration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login_Registration.Controllers
+{
+    public class MembersController : Controller
+    {
+        // GET: Members
+        public ActionResult Members()
+        {
+            return View(new List<Members>());
+        }
+    }
+}
---
+++ Models/Members.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Login_Registration.Models
+{
+     public class Members
+     {
+          public int Id { get; set; }
+          public string Name { get; set; }
+          public string Contact { get; set; }
+          public string JoiningDate { get; set; }
+          public string Plan { get; set; }
+          public int TrainerId { get; set; }
+     }
+}
